2422bb4f449587a463c67af92fe20404e6943a81
Vasyl Tykhan <tykhan@MacBook-Pro-Vasyl.local>
2020-09-08 17:20:00 +0300

    Updates

---
+++ precache-manifest.1482a85902bb07a429363ab6801eb480.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713fe25454badd0a67dbb52f949ecc6d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc78f67a2cba34648fe0",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "d88875f0efd980d916c5",
+    "url": "./static/css/main.db4a5331.chunk.css"
+  },
+  {
+    "revision": "dc78f67a2cba34648fe0",
+    "url": "./static/js/2.80eb7040.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.80eb7040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d88875f0efd980d916c5",
+    "url": "./static/js/main.379238ee.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
